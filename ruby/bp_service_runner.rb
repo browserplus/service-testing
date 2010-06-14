df7d6c6f0b00d4fbd6f7e3791b252da94f55ca42
Lloyd Hilaiel <lloyd@hilaiel.com>
2010-06-14 15:20:02 -0600

    new version of service runner harness that uses slave mode

--- ruby/bp_service_runner.rb
+++ ruby/bp_service_runner.rb
@@ -12,23 +12,26 @@ end
 
 module BrowserPlus
   module ProcessController
-    # perform a blocking read.  the third parameter is a magic duck:
-    # 1. if it evaluates to false, we'll block the full timeo
-    # 2. if it is a pattern, we'll  block until either timeo expires OR
-    #    we get output which matches the pattern
-    def mypread(pio, timeo, lookFor = false)
-      output = String.new
-      while nil != select( [ pio ], nil, nil, timeo )  
-        output += pio.sysread(1024) 
-        break if output.length && lookFor && output =~ lookFor
+    def getmsg(pio, timeo, lookFor = false)
+      j = nil
+      @outputbuffer = String.new if (@outputbuffer == nil)
+      while true
+        begin
+          # now peel off the first json map, taking advantage of the fact that
+          # ServiceRunner inserts newlines
+          m = @outputbuffer.match(/^([^\n]+)\n(.*)$/)
+          if m != nil
+            # regex pulls off that \n, so add it back!
+            @outputbuffer = "#{m[2]}\n";
+            j = JSON.parse(m[1])
+            return j
+          end
+        rescue
+        end
+        break if (nil == select( [ pio ], nil, nil, timeo ))  
+        @outputbuffer += pio.sysread(1024) 
       end
-      output
-    end
-
-    def mypread_raise(pio, timeo, lookFor = false)
-      str = mypread(pio, timeo, lookFor)
-      raise "read failed" if lookFor != false && str !~ lookFor
-      str
+      nil
     end
   end
 
@@ -38,31 +41,34 @@ module BrowserPlus
       raise "can't execute ServiceRunner: #{sr}" if !File.executable? sr
       cmd = ""
       if provider != nil
-        cmd = "#{sr} -providerPath #{provider} #{path}"
+        cmd = "#{sr} -slave -providerPath #{provider} #{path}"
       else
-        cmd = "#{sr} #{path}"
+        cmd = "#{sr} -slave #{path}"
       end
       @srp = IO.popen(cmd, "w+")
-      str = mypread_raise(@srp, 0.5, /service initialized/)
+      i = getmsg(@srp, 0.5)
+      raise i['msg'] if i && i['type'] == 'error' && i['msg']
+      raise "couldn't initialize" if i['msg'] !~ /service initialized/
       @instance = nil
     end
 
     # allocate a new instance
     def allocate
       @srp.syswrite "allocate\n"
-      str = mypread_raise(@srp, 3.0, /allocated/)
-      num = str.match(/allocated: (\d+)/)[1]
+      i = getmsg(@srp, 2.0)
+      raise "couldn't allocate" if !i.has_key?('msg')
+      num = i['msg']
       Instance.new(@srp, num)
     end
 
     # invoke a function on an automatically allocated instance of the service
-    def invoke f, a = nil
+    def invoke f, a, &cb
       @instance = allocate if @instance == nil
-      @instance.invoke f, a
+      @instance.invoke f, a, &cb
     end
 
-    def method_missing func, *args
-      invoke func, args[0]
+    def method_missing func, *args, &b
+      invoke func, args[0], &b
     end
 
     def shutdown
@@ -81,7 +87,7 @@ module BrowserPlus
     # SDK. (http://browserplus.yahoo.com)
     def findServiceRunner
       # first, try relative to this repo 
-      srBase = File.join(File.dirname(__FILE__), "..", "..", "bin")
+      srBase = File.join(File.dirname(__FILE__), "..", "..", "bpsdk", "bin")
       candidates = [
                     File.join(srBase, "ServiceRunner.exe"),
                     File.join(srBase, "ServiceRunner"),             
@@ -113,7 +119,7 @@ module BrowserPlus
       @srp = p
     end
 
-    def invoke func, args
+    def invoke func, args, &cb
       args = JSON.generate(args).gsub("'", "\\'")
       cmd = "inv #{func.to_s}"
       cmd += " '#{args}'" if args != "null"
@@ -121,18 +127,27 @@ module BrowserPlus
       # always select the current instance
       @srp.syswrite "select #{@iid}\n"
       @srp.syswrite cmd
-      str = mypread(@srp, 4.0, /^(?:> )?spawned process|^(?: ?\> )?error:|^(?:> )?".*"\n|^\}$|(?:^(?:> )?no such function: #{func.to_s}$)/m)
-      str = str.gsub(/^\> /m, "").gsub(/^ \> /m, "").gsub(/^\>/m, "")
-      raise str if str =~ /no such function: #{func.to_s}/ || str =~ /^error:/ || str =~ /^spawned/
-      JSON.parse("[#{str}]")[0]
+      while i = getmsg(@srp, 4.0)
+        # skip info messages
+        next if i['type'] == "info"
+        # invoke passed in block for callbacks?
+        if i['type'] == "callback"        
+          cb.call(i['msg']) if cb != nil
+          next
+        end
+        break
+      end
+      raise i['msg'] if i && i['type'] == 'error' && i['msg']
+      raise "invocation failure" if i == nil || i['type'] != 'results'
+      i['msg']
     end
 
     def destroy
       @srp.syswrite "destroy #{@iid}\n"
     end
 
-    def method_missing func, *args
-      invoke func, args[0]
+    def method_missing func, *args, &cb
+      invoke func, args[0], cb
     end
     private
     include ProcessController
